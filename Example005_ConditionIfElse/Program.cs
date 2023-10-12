110447f921bd9b6cc978822201cabeaca0d7f75d
Dmitry <prodemiks@mail.ru>
2023-10-12 08:00:35 +0300

    add Example 005 - Condition If-Else

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("ВВедите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "masha")
+{
+    Console.WriteLine("Ура! Это же МАША");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+} 
